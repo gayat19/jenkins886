d3606608cd587ed6de5929df92487dc279772bbb
gayat19 <35684683+gayat19@users.noreply.github.com>
2020-12-17 13:07:13 +0530

    Update Program.cs
    Commit after first build

--- SampleJenkinsProject/Program.cs
+++ SampleJenkinsProject/Program.cs
@@ -11,6 +11,7 @@ namespace SampleJenkinsProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello to jenkins");
+            Console.WriteLine("Added after first build")
         }
     }
 }
